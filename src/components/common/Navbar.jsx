60d767ae31886d69d967207c96e89ce6968a08b0
glowwEE <gloryokafor5@gmail.com>
2024-06-30 16:58:33 +0100

    Updated package.json to add react-icons dependency, modified Navbar.jsx to use react-icons.

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,34 +1,64 @@
+import { BiCart, BiHeart, BiSearch, BiUser } from "react-icons/bi"
+import { FaEnvelope, FaFacebook, FaInstagram, FaPhoneAlt, FaTwitter, FaYoutube } from "react-icons/fa"
+
 const navbar = () => {
   return (
     <nav className=" font-montserrat">
-      <div className="bg-[#23856D] text-white text-[13px] font-medium flex justify-between items-center h-[60px] px-[3rem]">
+      <div className="bg-[#23856D] text-white text-[13px] font-bold flex justify-between items-center h-[60px] px-[3rem]">
 
-        <div className="flex ">
-          <p className="contact mr-6">(225) 555-0118</p>
-          <p className="email ml-6">michelle.rivera@example.com</p>
+        <div className="flex">
+          <div className="flex justify-center items-center mr-9">
+            <div className=""><FaPhoneAlt/></div>
+            <p className="ml-2">  (225) 555-0118</p>
+          </div>
+          <div className="flex justify-center items-center">
+            <div><FaEnvelope/> </div>
+            <p className="ml-2"> michelle.rivera@example.com</p>
+          </div>
         </div>
 
         <div>Follow Us  and get a chance to win 80% off</div>
 
-        <div className="inline">Follow Us  :
-          <div className="inline-block mx-2">Twitter</div>
-          <div className="inline-block mr-1">instagram</div>
-          <div className="inline-block ml-1">youtube</div>
-          <div className="inline-block ml-2">facebook</div>
+        <div className="flex">
+          <div className="mr-3">Follow Us  :</div>
+          <div className="icon-container flex items-center justify-center">
+            <div className="inline-block mr-4">
+              <a href="https://www.instagram.com/">
+                <FaInstagram size={18}/>
+              </a>
+            </div>
+            <div className="inline-block mr-4">
+              <a href="https://www.youtube.com/">
+                <FaYoutube size={18}/>
+              </a>
+            </div>
+            <div className="inline-block mr-4">
+              <a href="https://www.facebook.com/">
+                <FaFacebook size={18}/>
+              </a>
+            </div>
+            <div className="inline-block mr-4">
+              <a href="https://www.x.com/">
+                <FaTwitter size={18}/>
+              </a>
+            </div>
+          </div>
+
         </div>
+
 
       </div>
 
       <div className="bg-white text-black flex justify-between items-center px-[3rem]">
 
         <div className="flex">
-          <div className="logo mr-[17%] text-xl font-bold">Bandage</div>
+          <div className="logo mr-[17%] text-xl font-bold"><a href="">Bandage</a></div>
 
           <div className="nav-item-container">
 
-            <ul className="nav-item flex text-[15px] font-medium">
+            <ul className="nav-item flex text-[15px] font-semibold">
               <li className="mx-1"> <a href="#">Home</a></li>
-              <li className="mx-1"><a href="#">Shop</a></li>
+              <li className="mx-1"><a href="#">Shop </a></li>
               <li className="mx-1"><a href="#">About</a></li>
               <li className="mx-1"><a href="#">Blog</a></li>
               <li className="mx-1"><a href="#">Contact</a></li>
@@ -38,12 +68,15 @@ const navbar = () => {
         </div>
 
 
-        <div className="font-medium  text-[15px] text-[#23A6F0]">
+        <div className="font-  text-[15px] text-[#23A6F0]">
             <ul className="flex justify-center items-center  h-[60px]">
-              <li className="mx-3"><a href="#">Login/Register</a> </li>
-              <li className="mx-3"><a href="#">search icon</a></li>
-              <li className="mx-3"><a href="#">cart icon</a></li>
-              <li className="mx-3"><a href="#">love icon</a></li>
+              <li className="mx-4  flex items-center ">
+                <a className="mr-1" href=""><BiUser size={22}/></a>
+                <a href="#">Login/Register</a>
+              </li>
+              <li className="mx-3"><a href="#"> <BiSearch size={22}/></a></li>
+              <li className="mx-3"><a href="#"><BiCart size={22}/></a></li>
+              <li className="mx-3"><a href="#"><BiHeart size={22}/></a></li>
             </ul>
         </div>
 
