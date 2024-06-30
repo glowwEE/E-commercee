7e7af2a71b65768a61ddc484683287e72995218b
glowwEE <gloryokafor5@gmail.com>
2024-06-30 13:29:18 +0100

    Updated grid layout and styling in HeroSection component

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -6,22 +6,22 @@ const HeroSection = () => {
   return (
 
     <div className=" h-2/6">
-      <div className="grid grid-cols-4 grid-rows-2 gap-3 h-screen max-w-[78%] m-auto py-16 ">
-        <div className="col-span-2 row-span-2 bg-cyan-400 relative  text-">
+      <div className="grid grid-cols-6 grid-rows-2 gap-3 h-screen max-w-[78%] m-auto py-16 ">
+        <div className="col-span-2 row-span-2 relative ">
           <img className="w-full h-full object-cover" src={HeroImg1} alt="image one" />
-          <HeroDetails itemsClass='text-[#2DC071]' titleClass='uppercase text-5xl leading-normal' readMoreClass='capitalize text-xs '/>
+          <HeroDetails itemsClass='text-[#2DC071]' titleClass='uppercase text-3xl leading-normal' readMoreClass='capitalize text-sm '/>
         </div>
-        <div className="col-span-2 bg-cyan-400  relative">
-          <img className="w-full h-full object-cover" src={HeroImg2} alt="" />
-          <HeroDetails itemsClass='text-[#2DC071] text-xs' titleClass='uppercase text-xl leading-relaxed' readMoreClass='capitalize text-xs '/>
+        <div className="col-span-4 relative">
+          <img className="w-full h-full " src={HeroImg2} alt="" />
+          <HeroDetails itemsClass='text-[#2DC071] text-xs' titleClass='uppercase text-2xl leading-relaxed' readMoreClass='capitalize text-xs '/>
           </div>
-        <div className="col-span-1 bg-cyan-400 relative">
+        <div className="col-span-2 relative">
           <img className="w-full h-full object-cover" src={HeroImg3} alt="" />
-          <HeroDetails itemsClass='text-[#2DC071] text-xs' titleClass='uppercase text-xl leading-relaxed' readMoreClass='capitalize text-xs '/>
+          <HeroDetails itemsClass='text-[#2DC071] text-xs' titleClass='uppercase text-2xl leading-relaxed' readMoreClass='capitalize text-xs '/>
         </div>
-        <div className="col-span-1 bg-cyan-400 flex items-center justify-center relative">
+        <div className="col-span-2 relative">
         <img className="w-full h-full object-cover" src={HeroImg4} alt="" />
-        <HeroDetails itemsClass='text-[#2DC071] text-xs' titleClass='uppercase text-xl leading-relaxed' readMoreClass='capitalize text-xs '/>
+        <HeroDetails itemsClass='text-[#2DC071] text-xs' titleClass='uppercase text-2xl leading-relaxed' readMoreClass='capitalize text-xs '/>
         </div>
       </div>
     </div>
